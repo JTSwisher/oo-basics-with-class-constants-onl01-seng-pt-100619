987ca7f0605799563690b35e6b6e93bfd51bc837
Jeff Swisher <jefftswisher@gmail.com>
2019-10-20 19:34:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,12 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  
+  GENRES = []
+  
+  def GENRES
+    genre.new << GENRES
+  end
 
   def initialize(title)
     @title = title
